5f1052e57e4ee5e53e72cafc8f8a14063b0fde6d
Aadil Mehraj <aadilmehraj76@gmail.com>
2020-12-01 00:21:35 +0530

    add base layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import AppLogo from './assets/app-logo.svg';
+
 function App() {
   return (
-    <div className="text-center text-xl pt-4 font-bold">
-      Hey TailwindCSS 2.0
+    <div className="h-screen flex">
+      <div className="? w-64 bg-gray-100 border-r">
+        <img class="h-12 w-12" src={AppLogo} alt="" />
+      </div>
+      <div className="bg-white">
+        <div className=""></div>
+        <div></div>
+      </div>
     </div>
   );
 }
